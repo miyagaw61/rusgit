d14d0c06407a27e0a0948a5a24a547e75faba583
miyagaw61 <miyagaw61@gmail.com>
2018-01-10 21:48:31 +0900

    add color

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 extern crate clap;
+extern crate colored;
+extern crate regex;
 
 use clap::{App, Arg, SubCommand};
 use std::process::Command;
+use colored::*;
+use regex::Regex;
 
 fn shell(command: &str) -> String {
     let output = Command::new("sh")
@@ -9,15 +13,57 @@ fn shell(command: &str) -> String {
         .arg(command)
         .output()
         .expect("failed to execute process");
-    return std::str::from_utf8(&output.stdout[..]).unwrap().to_string()
+    std::str::from_utf8(&output.stdout[..]).unwrap().to_string()
+}
+
+fn regex(re_str: &str) -> Regex {
+    Regex::new(re_str).unwrap()
 }
 
 fn status() -> String {
-    return shell("git status --short")
+    let result = shell("git status --short");
+    let result = regex(r"\n$").replace(result.as_str(), "");
+    let lines: Vec<&str> = result.split("\n").collect();
+    let mut result_vec: Vec<String> = Vec::new();
+    for x in lines {
+        let x_array: Vec<char> = x.chars().collect();
+        let mut r0: String = " ".to_string();
+        let mut r1: String = " ".to_string();
+        if x_array[0] == 'M' {
+            r0 = "M".green().bold().to_string();
+        }
+        if x_array[1] == 'M' {
+            r1 = "M".red().bold().to_string();
+        }
+        if x_array[0] == 'A' {
+            r0 = "A".green().bold().to_string();
+        }
+        if x_array[0] == 'A' {
+            r0 = "A".green().bold().to_string();
+        }
+        if x_array[0] == '?' && x_array[1] == '?'{
+            r0 = "?".red().bold().to_string();
+            r1 = "?".red().bold().to_string();
+        }
+        let x_string: String = x_array[2..].into_iter().collect();
+        let result_line: String = [r0, r1, x_string].join("");
+        result_vec.push(result_line);
+    }
+    result_vec.join("\n")
+}
+
+fn status_routine(){
+    println!("{}", status());
 }
 
 fn add(files: Vec<&str>) {
     shell(["git add", files.join(" ").as_str()].join(" ").as_str());
+}
+
+fn add_routine(matches: &clap::ArgMatches) {
+    let files: Vec<&str> = matches.subcommand_matches("add").unwrap().values_of("files").unwrap().collect();
+    add(files);
+    println!("{}", status());
 }
 
 fn main() {
@@ -38,11 +84,10 @@ fn main() {
                     )
         .get_matches();
 
-    if matches.is_present("status") {
-        println!("{}", status());
-    } else if let Some(ref matches) = matches.subcommand_matches("add") {
-        let files: Vec<&str> = matches.values_of("files").unwrap().collect();
-        add(files);
-        println!("{}", status());
-    }
+    let sub_command = matches.subcommand_name().unwrap_or("");
+    match sub_command {
+        "status" => status_routine(),
+        "add" => add_routine(&matches),
+        _ => println!("something else.")
+    } 
 }
